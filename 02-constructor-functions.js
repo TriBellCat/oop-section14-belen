2520b89bd6335dd3adcaf2db04c246f9fb2d39fb
TriBellCat <t_belen@u.pacific.edu>
2024-05-07 18:54:09 -0700

    Added in the code that I did along

---
+++ 02-constructor-functions.js
@@ -0,0 +1,23 @@
+function Rectangle(name, width, height) {
+  this.name = name;
+  this.width = width;
+  this.height = height;
+
+  this.area = function () {
+    return height * width;
+  };
+}
+
+const rect1 = new Rectangle("Rectangle 1", 10, 10);
+console.log(rect1.name);
+console.log(rect1.area());
+
+const rect2 = new Rectangle("Rectangle 2", 20, 10);
+const rect3 = new Rectangle("Rectangle 3", 30, 30);
+
+console.log(rect2.name, rect3.name);
+console.log(rect2.area(), rect3.area());
+
+console.log(rect1.constructor);
+
+console.log(rect2 instanceof Rectangle);
